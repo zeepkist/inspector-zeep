c5c74b93c93d22922e87fb89a231a8567145966b
wopian <wopian@wopian.me>
2024-04-16 20:33:12 +0100

    fix: update theme requirements

--- src/config/requirements.ts
+++ src/config/requirements.ts
@@ -1,7 +1,7 @@
 export const BLOCK_LIMIT = 4000 // 4000
-export const MINIMUM_TIME = 30 // seconds
-export const MAXIMUM_TIME = 75 // seconds
+export const MINIMUM_TIME = 20 // seconds
+export const MAXIMUM_TIME = 30 // seconds
 export const MINIMUM_CHECKPOINTS = 2
 // eslint-disable-next-line @typescript-eslint/no-inferrable-types
 export const MAXIMUM_WIDTH: number = 0 // x 1x1 blocks wide
-export const START_FINISH_PROXIMITY: number = 16 * 5 // 3 blocks apart
+export const START_FINISH_PROXIMITY: number = 0 // 5 blocks apart (16 * 5)
