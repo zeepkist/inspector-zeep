cc5d9a93b98e56fe8404794667af2be4976a660a
James Harris <3440094+wopian@users.noreply.github.com>
2025-03-06 00:58:57 +0000

    chore: increase required checkpoints to 3 and max author time down to 60s
    Signed-off-by: James Harris <3440094+wopian@users.noreply.github.com>

--- src/config/requirements.ts
+++ src/config/requirements.ts
@@ -1,7 +1,7 @@
 export const BLOCK_LIMIT = 4000 // 4000
 export const MINIMUM_TIME = 20 // seconds
-export const MAXIMUM_TIME = 75 // seconds
-export const MINIMUM_CHECKPOINTS = 2
+export const MAXIMUM_TIME = 60 // seconds
+export const MINIMUM_CHECKPOINTS = 3
 // eslint-disable-next-line @typescript-eslint/no-inferrable-types
 export const MAXIMUM_WIDTH: number = 0 // x 1x1 blocks wide
 export const START_FINISH_PROXIMITY: number = 0 // 5 blocks apart (16 * 5)
