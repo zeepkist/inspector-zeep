754ddfb5facced405022ea3e2b4db8b978ce9f39
James Harris <3440094+wopian@users.noreply.github.com>
2025-03-23 20:38:11 +0000

    feat: only remove reactions if level validity has changed

--- src/reactToMessage.ts
+++ src/reactToMessage.ts
@@ -2,32 +2,39 @@ import { Message } from 'discord.js'
 
 import { error, info } from './log.js'
 
+const REACTION_IDS = {
+  YES: '1080204636583104573',
+  NO: '1080204670418558987'
+}
+
+const REACTION_EMOJIS = {
+  YES: '<:zk_yes:1080204636583104573>',
+  NO: '<:zk_no:1080204670418558987>'
+}
+
 export const reactToMessage = async (message: Message, isValid: boolean) => {
   try {
-    const reactionToRemove = isValid
-      ? '1080204670418558987'
-      : '1080204636583104573'
+    const reactionEmoji = isValid ? REACTION_EMOJIS.YES : REACTION_EMOJIS.NO
+    const reactionToAdd = isValid ? REACTION_IDS.YES : REACTION_IDS.NO
+    const reactionToRemove = isValid ? REACTION_IDS.NO : REACTION_IDS.YES
 
-    const reactions = message.reactions.cache.filter(
-      reaction => reaction.emoji.id === reactionToRemove
+    const currentReaction = message.reactions.cache.find(
+      reaction => reaction.emoji.id === reactionToAdd
     )
 
-    for (const reaction of reactions.values()) {
-      await reaction.remove()
+    if (!currentReaction) {
+      const reactionsToRemove = message.reactions.cache.filter(
+        reaction => reaction.emoji.id === reactionToRemove
+      )
+
+      for (const reaction of reactionsToRemove.values()) {
+        await reaction.remove()
+      }
+
+      info(`Reacting to message with ${reactionEmoji}`, import.meta)
+
+      await message.react(reactionEmoji)
     }
-
-    info(
-      `Reacting to message with ${
-        isValid
-          ? '<:zk_yes:1080204636583104573>'
-          : '<:zk_no:1080204670418558987>'
-      }`,
-      import.meta
-    )
-
-    message.react(
-      isValid ? '<:zk_yes:1080204636583104573>' : '<:zk_no:1080204670418558987>'
-    )
   } catch (error_) {
     error(`Error reacting to message: ${error_}`, import.meta)
   }
